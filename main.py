4704e618cb0df32b097f388fe1963a456cb63ea4
Mmachi Ezeh <mmachilawri@gmail.com>
2024-06-21 14:27:05 +0100

    added images and links

--- main.py
+++ main.py
@@ -21,14 +21,23 @@ with col2:
 content1 = """ Below you can find some of the apps i have built in python. feel free to contact me!"""
 st.write(content1)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 df = pandas.read_csv("data.csv", sep= ";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
-
+        st.write(row["description"])
+        st.image(row["image"])
+        st.write(f"[source code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(row["image"])
+        # st.image('images/' + row['image']) this is if you created a folder.
+        st.write(f"[source code]({row['url']})")
+
+
+
